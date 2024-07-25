407bb00dd3fa53b55a480aada9c8fe2f72324d90
oswaldo chilin <oswaldochilin49@gmail.com>
2024-07-24 21:06:04 -0600

    Initial 3

---
+++ npants/src/main/java/com/npants/npants/NpantsApplication.java
@@ -0,0 +1,13 @@
+package com.npants.npants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NpantsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NpantsApplication.class, args);
+	}
+
+}
---
+++ npants/src/test/java/com/npants/npants/NpantsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.npants.npants;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NpantsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
